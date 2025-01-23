77680b542da0f8ca3786390954adfd5c6d3c4407
BugHunter-X <askzenai@gmail.com>
2025-01-23 18:48:04 +0100

    Add bugSolution.py file

---
+++ bugSolution.py
@@ -0,0 +1,8 @@
+def function(a, b):
+    if b == 0:
+        return 0  # Or raise a more descriptive exception
+    else:
+        return a / b
+
+result = function(10, 0)
+print(result) # Output: 0
